19bb8a411b572d4e13586c97fedc0fc3a00b5d4e
Elgun Mehdi <elgun88@gmail.com>
2023-12-19 12:48:20 -0600

    Restructure main section and implement remove book

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const mainSection = document.querySelector('.main-section');
+const bookList = document.querySelector('.book-list');
+const statusSpan = document.querySelectorAll('.status');
+
+const checkReadingStatus = function (elementArr) {
+  elementArr.forEach(element => {
+    element.style.backgroundColor = `${
+      element.textContent === 'Read' ? '#caffbf' : '#ffadad'
+    }`;
+  });
+
+  return elementArr;
+};
+
+checkReadingStatus(statusSpan);
+
+mainSection.addEventListener('click', function (e) {
+  const target = e.target;
+  if (!target.classList.contains('action')) return;
+
+  if (target.classList.contains('delete')) {
+    bookList.removeChild(target.closest('.book'));
+  }
+});
+
+//Photo by <a href="https://unsplash.com/@chuttersnap?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">CHUTTERSNAP</a> on <a href="https://unsplash.com/photos/assorted-title-book-lot-Zf64Osndqvc?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
