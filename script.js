6c5210643fe30760bf33ff817b845aad1e9c083d
Elgun Mehdi <elgun88@gmail.com>
2023-12-20 18:25:36 -0600

    Implement save on edit and change reading status effect

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ const bookList = document.querySelector('.book-list');
 const statusParagraphs = document.querySelectorAll('.status');
 
 const myLibrary = [];
+let editedBookIndex;
 let isBookAdded = false;
 
 function Book(title, author, genre, pages, isRead) {
@@ -25,21 +26,19 @@ function Book(title, author, genre, pages, isRead) {
   this.isRead = isRead;
 }
 
-const addBookToLibrary = function (library) {
-  library.push(
-    new Book(
-      inputTitle.value,
-      inputAuthor.value,
-      inputGenre.value,
-      +inputPages.value,
-      inputReadingStatus.checked
-    )
+const addNewBookToLibrary = function (library) {
+  const newBook = new Book(
+    inputTitle.value,
+    inputAuthor.value,
+    inputGenre.value,
+    +inputPages.value,
+    inputReadingStatus.checked
   );
+  library.push(newBook);
   return library;
 };
 
-const generateBook = function (library, i) {
-  const bookObj = library[i];
+const generateBookHTML = function (bookObj, i) {
   const html = `<div class="book" data-bookIndex=${i}>
      <p>${bookObj.title}</p>
      <p>${bookObj.author}</p>
@@ -69,6 +68,17 @@ const generateBook = function (library, i) {
   return html;
 };
 
+const renderAllBooks = function (library, bookList) {
+  const allBookElements = document.querySelectorAll('.book');
+  allBookElements.forEach(bookEl => bookList.removeChild(bookEl));
+
+  library.forEach((book, i) => {
+    const newBookHTML = generateBookHTML(book, i);
+    bookList.insertAdjacentHTML('beforeend', newBookHTML);
+  });
+  setBgColorBasedOnReadingStatus(document.querySelectorAll('.status'));
+};
+
 const removeBookFromLibrary = function (bookElement, library) {
   library.forEach((_, i) => {
     if (+bookElement.dataset.bookindex === i) {
@@ -76,11 +86,6 @@ const removeBookFromLibrary = function (bookElement, library) {
     }
   });
   return library;
-};
-
-const removeBookFromDOM = function (bookElement, bookList) {
-  bookList.removeChild(bookElement);
-  return bookElement;
 };
 
 const clearInputs = function () {
@@ -121,17 +126,34 @@ mainSection.addEventListener('click', function (e) {
       e.clientX > dialogElementPosition.right ||
       e.clientY < dialogElementPosition.top ||
       e.clientY > dialogElementPosition.bottom
-    )
+    ) {
       dialog.close();
+      if (addBookBtn.style.display === 'none') {
+        saveBtn.style.display = 'none';
+        addBookBtn.style.display = 'block';
+      }
+    }
+  }
+
+  if (target.classList.contains('open-modal')) {
+    clearInputs();
+    dialog.showModal();
+  }
+
+  if (target.classList.contains('add-book')) {
+    const updatedLibrary = addNewBookToLibrary(myLibrary);
+
+    renderAllBooks(updatedLibrary, bookList);
   }
 
   if (target.classList.contains('edit')) {
     const bookElement = target.closest('.book');
+    editedBookIndex = +bookElement.dataset.bookindex;
 
     addBookBtn.style.display = 'none';
     saveBtn.style.display = 'block';
     myLibrary.forEach((book, i) => {
-      if (+bookElement.dataset.bookindex === i) {
+      if (editedBookIndex === i) {
         inputTitle.value = book.title;
         inputAuthor.value = book.author;
         inputGenre.value = book.genre;
@@ -142,22 +164,36 @@ mainSection.addEventListener('click', function (e) {
     dialog.showModal();
   }
 
+  if (target.classList.contains('save')) {
+    const newBookObj = new Book(
+      inputTitle.value,
+      inputAuthor.value,
+      inputGenre.value,
+      +inputPages.value,
+      inputReadingStatus.checked
+    );
+    myLibrary.splice(editedBookIndex, 1, newBookObj);
+    renderAllBooks(myLibrary, bookList);
+  }
+
   if (target.classList.contains('remove')) {
     const bookElement = target.closest('.book');
-    removeBookFromDOM(bookElement, bookList);
-    removeBookFromLibrary(bookElement, myLibrary);
+    const updatedLibrary = removeBookFromLibrary(bookElement, myLibrary);
+
+    renderAllBooks(updatedLibrary, bookList);
   }
 
-  if (target.classList.contains('open-modal')) {
-    clearInputs();
-    dialog.showModal();
-  }
-
-  if (target.classList.contains('add-book')) {
-    const updatedLibrary = addBookToLibrary(myLibrary);
-    const bookHTML = generateBook(updatedLibrary, updatedLibrary.length - 1);
-    bookList.insertAdjacentHTML('beforeend', bookHTML);
-    setBgColorBasedOnReadingStatus(document.querySelectorAll('.status'));
+  if (target.classList.contains('status')) {
+    const bookElement = target.closest('.book');
+    myLibrary.forEach((book, i) => {
+      if (+bookElement.dataset.bookindex === i) {
+        book.isRead = !book.isRead;
+        target.textContent = book.isRead ? 'Read' : 'Not read';
+        target.style.backgroundColor = `${
+          target.textContent === 'Read' ? '#caffbf' : '#ffadad'
+        }`;
+      }
+    });
   }
 });
 
